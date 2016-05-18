3de0298a01cff9f302522db928b177b86c7d9c5f
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-18 16:24:35 +1000

    Add generic icon for testing

---
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineIcons.java
@@ -0,0 +1,9 @@
+package com.jeremyworboys.expressionengine;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class ExpressionEngineIcons {
+    public static final Icon FILE = IconLoader.getIcon("/com/jeremyworboys/expressionengine/icons/jar-gray.png");
+}
