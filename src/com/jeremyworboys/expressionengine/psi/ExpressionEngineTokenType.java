5d8c6043e3993358a9eb43895e490243f85f6f87
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-18 16:41:50 +1000

    Define a token type

---
+++ src/com/jeremyworboys/expressionengine/psi/ExpressionEngineTokenType.java
@@ -0,0 +1,17 @@
+package com.jeremyworboys.expressionengine.psi;
+
+import com.intellij.psi.tree.IElementType;
+import com.jeremyworboys.expressionengine.ExpressionEngineLanguage;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class ExpressionEngineTokenType extends IElementType {
+    public ExpressionEngineTokenType(@NotNull @NonNls String debugName) {
+        super(debugName, ExpressionEngineLanguage.INSTANCE);
+    }
+
+    @Override
+    public String toString() {
+        return "ExpressionEngineTokenType." + super.toString();
+    }
+}
