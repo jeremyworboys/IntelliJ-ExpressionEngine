ec2f0302c5a25d32600d838084ebe0977c01875f
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-18 16:29:39 +1000

    Add file type class

---
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineFileType.java
@@ -0,0 +1,39 @@
+package com.jeremyworboys.expressionengine;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class ExpressionEngineFileType extends LanguageFileType {
+    public static final ExpressionEngineFileType INSTANCE = new ExpressionEngineFileType();
+
+    private ExpressionEngineFileType() {
+        super(ExpressionEngineLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "ExpressionEngine file";
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "ExpressionEngine language file";
+    }
+
+    @NotNull
+    @Override
+    public String getDefaultExtension() {
+        return "html";
+    }
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return ExpressionEngineIcons.FILE;
+    }
+}
