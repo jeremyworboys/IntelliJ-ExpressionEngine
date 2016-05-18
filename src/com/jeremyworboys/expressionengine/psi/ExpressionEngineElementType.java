25fa237059534a95dd3759ddc8701b895795f012
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-18 16:42:54 +1000

    Define an element type

---
+++ src/com/jeremyworboys/expressionengine/psi/ExpressionEngineElementType.java
@@ -0,0 +1,12 @@
+package com.jeremyworboys.expressionengine.psi;
+
+import com.intellij.psi.tree.IElementType;
+import com.jeremyworboys.expressionengine.ExpressionEngineLanguage;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class ExpressionEngineElementType extends IElementType {
+    public ExpressionEngineElementType(@NotNull @NonNls String debugName) {
+        super(debugName, ExpressionEngineLanguage.INSTANCE);
+    }
+}
