4ed8a9f6c1e59a8e214419074cd3bc3530d595ca
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-19 19:45:30 +1000

    Add layered editor highlighter

---
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineEditorHighlighter.java
@@ -0,0 +1,39 @@
+package com.jeremyworboys.expressionengine;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.ex.util.LayerDescriptor;
+import com.intellij.openapi.editor.ex.util.LayeredLexerEditorHighlighter;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.templateLanguages.TemplateDataLanguageMappings;
+import com.jeremyworboys.expressionengine.psi.ExpressionEngineTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ExpressionEngineEditorHighlighter extends LayeredLexerEditorHighlighter {
+    public ExpressionEngineEditorHighlighter(@Nullable Project project, @Nullable VirtualFile virtualFile, @NotNull EditorColorsScheme colors) {
+        super(new ExpressionEngineSyntaxHighlighter(), colors);
+
+        FileType fileType = null;
+        if (project == null || virtualFile == null) {
+            fileType = StdFileTypes.PLAIN_TEXT;
+        } else {
+            Language language = TemplateDataLanguageMappings.getInstance(project).getMapping(virtualFile);
+            if (language != null) {
+                fileType = language.getAssociatedFileType();
+            }
+            if (fileType == null) {
+                fileType = StdFileTypes.HTML;
+            }
+        }
+
+        SyntaxHighlighter outerHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(fileType, project, virtualFile);
+
+        registerLayer(ExpressionEngineTypes.HTML, new LayerDescriptor(outerHighlighter, ""));
+    }
+}
--- src/com/jeremyworboys/expressionengine/ExpressionEngineFileType.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineFileType.java
@@ -1,6 +1,13 @@
 package com.jeremyworboys.expressionengine;
 
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.highlighter.EditorHighlighter;
+import com.intellij.openapi.fileTypes.EditorHighlighterProvider;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeEditorHighlighterProviders;
 import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,6 +20,13 @@ public class ExpressionEngineFileType extends LanguageFileType {
 
     private ExpressionEngineFileType() {
         super(ExpressionEngineLanguage.INSTANCE);
+
+        FileTypeEditorHighlighterProviders.INSTANCE.addExplicitExtension(this, new EditorHighlighterProvider() {
+            @Override
+            public EditorHighlighter getEditorHighlighter(@Nullable Project project, @NotNull FileType fileType, @Nullable VirtualFile virtualFile, @NotNull EditorColorsScheme colors) {
+                return new ExpressionEngineEditorHighlighter(project, virtualFile, colors);
+            }
+        });
     }
 
     @NotNull
