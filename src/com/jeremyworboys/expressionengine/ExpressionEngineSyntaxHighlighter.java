40f5d85536bd7a4589024dc4f880d55b35cd83f4
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-19 14:20:35 +1000

    Add syntax highlighting for numbers and strings

--- src/com/jeremyworboys/expressionengine/ExpressionEngineSyntaxHighlighter.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineSyntaxHighlighter.java
@@ -12,8 +12,12 @@ import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAtt
 
 public class ExpressionEngineSyntaxHighlighter extends SyntaxHighlighterBase {
     public static final TextAttributesKey IDENTIFIER = createTextAttributesKey("EE_IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER);
+    public static final TextAttributesKey NUMBER = createTextAttributesKey("EE_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
+    public static final TextAttributesKey STRING = createTextAttributesKey("EE_STRING", DefaultLanguageHighlighterColors.STRING);
 
     private static final TextAttributesKey[] IDENTIFIER_KEYS = new TextAttributesKey[]{IDENTIFIER};
+    private static final TextAttributesKey[] NUMBER_KEYS = new TextAttributesKey[]{NUMBER};
+    private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
     private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
 
     @NotNull
@@ -27,8 +31,18 @@ public class ExpressionEngineSyntaxHighlighter extends SyntaxHighlighterBase {
     public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
         if (tokenType.equals(ExpressionEngineTypes.IDENTIFIER)) {
             return IDENTIFIER_KEYS;
+        } else if (tokenType.equals(ExpressionEngineTypes.NUMBER)) {
+            return NUMBER_KEYS;
+        } else if (isPartOfString(tokenType)) {
+            return STRING_KEYS;
         } else {
             return EMPTY_KEYS;
         }
+    }
+
+    private static boolean isPartOfString(IElementType tokenType) {
+        return tokenType.equals(ExpressionEngineTypes.STRING)
+            || tokenType.equals(ExpressionEngineTypes.STRING_START)
+            || tokenType.equals(ExpressionEngineTypes.STRING_END);
     }
 }
