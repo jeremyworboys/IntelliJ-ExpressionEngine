a499ed2d91739fa9fba200214fa959828d7fe97d
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-21 22:28:01 +1000

    Add annotator class

---
+++ src/com/jeremyworboys/expressionengine/annotator/ExpressionEngineAnnotator.java
@@ -0,0 +1,13 @@
+package com.jeremyworboys.expressionengine.annotator;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+
+public class ExpressionEngineAnnotator implements Annotator {
+    @Override
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+
+    }
+}
