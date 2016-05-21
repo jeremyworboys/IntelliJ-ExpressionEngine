e1c615488fafee14109f13fa6afabc87319af6f1
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-21 23:23:46 +1000

    Annotate missing referenced templates

--- src/com/jeremyworboys/expressionengine/annotator/ExpressionEngineAnnotator.java
+++ src/com/jeremyworboys/expressionengine/annotator/ExpressionEngineAnnotator.java
@@ -3,11 +3,30 @@ package com.jeremyworboys.expressionengine.annotator;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.psi.PsiElement;
+import com.jeremyworboys.expressionengine.psi.ExpressionEngineFile;
+import com.jeremyworboys.expressionengine.util.ExpressionEngineUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class ExpressionEngineAnnotator implements Annotator {
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        annotateTemplateReference(element, holder);
+    }
 
+    private void annotateTemplateReference(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        if (!ExpressionEngineUtil.getTemplateFileReferencePattern().accepts(element)) {
+            return;
+        }
+
+        String templateName = element.getText();
+        List<ExpressionEngineFile> templateFiles = ExpressionEngineUtil.getTemplateFiles(element.getProject(), templateName);
+
+        if (templateFiles.size() > 0) {
+            return;
+        }
+
+        holder.createWarningAnnotation(element, "Missing Template");
     }
 }
---
+++ src/com/jeremyworboys/expressionengine/util/ExpressionEngineUtil.java
@@ -0,0 +1,101 @@
+package com.jeremyworboys.expressionengine.util;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.patterns.ElementPattern;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.indexing.FileBasedIndex;
+import com.jeremyworboys.expressionengine.ExpressionEngineFileType;
+import com.jeremyworboys.expressionengine.ExpressionEngineLanguage;
+import com.jeremyworboys.expressionengine.psi.ExpressionEngineFile;
+import com.jeremyworboys.expressionengine.psi.ExpressionEngineTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ExpressionEngineUtil {
+    @NotNull
+    public static ElementPattern<PsiElement> getTemplateFileReferencePattern() {
+        return getTemplateFileReferencePattern("layout", "embed");
+    }
+
+    @NotNull
+    public static ElementPattern<PsiElement> getTemplateFileReferencePattern(String... tagNames) {
+        // Matches: {embed="<xxx>"}
+        //noinspection unchecked
+        ElementPattern<PsiElement> stringPattern = PlatformPatterns
+            .psiElement(ExpressionEngineTypes.T_STRING)
+            .afterLeafSkipping(
+                PlatformPatterns.or(
+                    PlatformPatterns.psiElement(ExpressionEngineTypes.T_LD),
+                    PlatformPatterns.psiElement(ExpressionEngineTypes.T_EQUALS),
+                    PlatformPatterns.psiElement(ExpressionEngineTypes.T_STRING_START)
+                ),
+                PlatformPatterns.psiElement(ExpressionEngineTypes.T_PARAM_VAR).withText(PlatformPatterns.string().oneOf(tagNames))
+            )
+            .withLanguage(ExpressionEngineLanguage.INSTANCE);
+
+        // Matches: {embed=<xxx>}
+        //noinspection unchecked
+        ElementPattern<PsiElement> pathPattern = PlatformPatterns
+            .psiElement(ExpressionEngineTypes.T_PATH)
+            .afterLeafSkipping(
+                PlatformPatterns.or(
+                    PlatformPatterns.psiElement(ExpressionEngineTypes.T_LD),
+                    PlatformPatterns.psiElement(ExpressionEngineTypes.T_EQUALS)
+                ),
+                PlatformPatterns.psiElement(ExpressionEngineTypes.T_PARAM_VAR).withText(PlatformPatterns.string().oneOf(tagNames))
+            )
+            .withLanguage(ExpressionEngineLanguage.INSTANCE);
+
+        //noinspection unchecked
+        return PlatformPatterns.or(stringPattern, pathPattern);
+    }
+
+    @NotNull
+    public static List<ExpressionEngineFile> getTemplateFiles(@NotNull Project project, String templateName) {
+        List<ExpressionEngineFile> result = new ArrayList<>();
+
+        GlobalSearchScope scope = GlobalSearchScope.allScope(project);
+        Collection<VirtualFile> virtualFiles = FileBasedIndex.getInstance()
+            .getContainingFiles(FileTypeIndex.NAME, ExpressionEngineFileType.INSTANCE, scope);
+
+        for (VirtualFile virtualFile : virtualFiles) {
+            if (isMatchingTemplateName(virtualFile, templateName)) {
+                ExpressionEngineFile expressionEngineFile = (ExpressionEngineFile) PsiManager.getInstance(project).findFile(virtualFile);
+                if (expressionEngineFile != null) {
+                    result.add(expressionEngineFile);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean isMatchingTemplateName(VirtualFile virtualFile, String templateName) {
+        String templatePath = toTemplatePath(templateName);
+        if (templatePath != null) {
+            return virtualFile.getPath().endsWith(templatePath);
+        }
+
+        return false;
+    }
+
+    @Nullable
+    private static String toTemplatePath(String templateName) {
+        String[] parts = templateName.split("\\/");
+
+        if (parts.length == 2) {
+            return parts[0] + ".group/" + parts[1] + ".html";
+        }
+
+        return null;
+    }
+}
