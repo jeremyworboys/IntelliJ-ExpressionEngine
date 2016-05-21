aeb3206ad04fa62d8a157b06abc1034c18a60805
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-22 00:59:52 +1000

    Add notes about other potential annotations

--- src/com/jeremyworboys/expressionengine/annotator/ExpressionEngineAnnotator.java
+++ src/com/jeremyworboys/expressionengine/annotator/ExpressionEngineAnnotator.java
@@ -14,9 +14,22 @@ public class ExpressionEngineAnnotator implements Annotator {
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
         annotateTemplateReference(element, holder);
+        // TODO: in conditional expression the string "0" is considered TRUE since it is a non-empty string
+        // TODO: in conditional expression negation happens after exponentiation (-5 ** 2 == -25 vs (-5) **2 == 25)
+        // TODO: right expr after "matches" operator in conditional expression must be a valid regular expression
+        // TODO: layout tag must come before any module or plugin tags
+        // TODO: {layout:contents} is reserved for the template data (annotate if used in {layout=""} tag)
+        // TODO: using {layout:set} in a looping tag pair will populate the value of the layout variable with the last element of the pair
+        // TODO: using {redirect} outside of a conditional body (unless it is the only tag) is likely a mistake
+        // TODO: multiple {paginate} blocks with different content need to use paginate=inline
+        // TODO: missing module params (e.g. channel:entries[disable])
+        // TODO: recommended module params (e.g. channel:entries[disable], current_time[format])
+        // TODO: undefined module params (parse module file for $tmpl->fetch_param()?)
+        // TODO: fixed set of param values (e.g. paginate[paginate])
     }
 
     private void annotateTemplateReference(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        // TODO: Check {path}, {route}, {redirect} but consider special cases for each
         if (!ExpressionEngineUtil.getTemplateFileReferencePattern().accepts(element)) {
             return;
         }
