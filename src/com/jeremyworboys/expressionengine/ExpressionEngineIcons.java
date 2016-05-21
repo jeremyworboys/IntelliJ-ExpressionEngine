b639fe5ca11494185748bb94069843fed0b9f051
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-21 15:51:47 +1000

    Add custom file icon

--- src/com/jeremyworboys/expressionengine/ExpressionEngineIcons.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class ExpressionEngineIcons {
-    public static final Icon FILE = IconLoader.getIcon("/com/jeremyworboys/expressionengine/icons/jar-gray.png");
+    public static final Icon FILE = IconLoader.getIcon("/com/jeremyworboys/expressionengine/icons/fileIcon.png");
 }
