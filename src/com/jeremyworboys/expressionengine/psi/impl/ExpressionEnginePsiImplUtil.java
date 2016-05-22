65e5529a027df8a08486d358d603c2da9300834d
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-23 00:15:47 +1000

    Fix get reference/s implementation

--- src/com/jeremyworboys/expressionengine/psi/impl/ExpressionEnginePsiImplUtil.java
+++ src/com/jeremyworboys/expressionengine/psi/impl/ExpressionEnginePsiImplUtil.java
@@ -1,29 +1,4 @@
 package com.jeremyworboys.expressionengine.psi.impl;
 
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiReferenceService;
-import com.intellij.psi.tree.IElementType;
-import com.jeremyworboys.expressionengine.psi.ExpressionEngineTagParamValue;
-import com.jeremyworboys.expressionengine.psi.ExpressionEngineTypes;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 public class ExpressionEnginePsiImplUtil {
-    @Nullable
-    public static PsiReference getReference(ExpressionEngineTagParamValue element) {
-        PsiReference[] references = getReferences(element);
-        if (references.length > 0) {
-            return references[0];
-        }
-        return null;
-    }
-
-    @NotNull
-    public static PsiReference[] getReferences(ExpressionEngineTagParamValue element) {
-        IElementType type = element.getNode().getFirstChildNode().getElementType();
-        if (type != ExpressionEngineTypes.T_PATH || type != ExpressionEngineTypes.STRING_LITERAL) {
-            return PsiReference.EMPTY_ARRAY;
-        }
-        return PsiReferenceService.getService().getContributedReferences(element);
-    }
 }
---
+++ src/com/jeremyworboys/expressionengine/psi/impl/ExpressionEngineTagParamValueMixin.java
@@ -0,0 +1,36 @@
+package com.jeremyworboys.expressionengine.psi.impl;
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
+import com.intellij.psi.tree.IElementType;
+import com.jeremyworboys.expressionengine.psi.ExpressionEngineTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class ExpressionEngineTagParamValueMixin extends ASTWrapperPsiElement {
+    public ExpressionEngineTagParamValueMixin(@NotNull ASTNode node) {
+        super(node);
+    }
+
+    @Override
+    @Nullable
+    public PsiReference getReference() {
+        PsiReference[] references = getReferences();
+        if (references.length > 0) {
+            return references[0];
+        }
+        return null;
+    }
+
+    @Override
+    @NotNull
+    public PsiReference[] getReferences() {
+        IElementType type = this.getNode().getFirstChildNode().getElementType();
+        if (type != ExpressionEngineTypes.T_PATH && type != ExpressionEngineTypes.STRING_LITERAL) {
+            return PsiReference.EMPTY_ARRAY;
+        }
+        return ReferenceProvidersRegistry.getReferencesFromProviders(this);
+    }
+}
