1b2c98cece1ca2bb7e029e19ec83a804d548c8ae
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-23 00:16:04 +1000

    Add empty template reference contributor class

---
+++ src/com/jeremyworboys/expressionengine/referencing/TemplateReferenceContributor.java
@@ -0,0 +1,12 @@
+package com.jeremyworboys.expressionengine.referencing;
+
+import com.intellij.psi.PsiReferenceContributor;
+import com.intellij.psi.PsiReferenceRegistrar;
+import org.jetbrains.annotations.NotNull;
+
+public class TemplateReferenceContributor extends PsiReferenceContributor {
+    @Override
+    public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
+
+    }
+}
