0168135dfe009aba38616dc52242a07048c21319
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-23 11:36:16 +1000

    Add element rename implementation

--- src/com/jeremyworboys/expressionengine/referencing/TemplateReference.java
+++ src/com/jeremyworboys/expressionengine/referencing/TemplateReference.java
@@ -3,6 +3,7 @@ package com.jeremyworboys.expressionengine.referencing;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementResolveResult;
 import com.intellij.psi.PsiReferenceBase;
@@ -42,7 +43,18 @@ public class TemplateReference extends PsiReferenceBase.Poly<PsiElement> {
 
     @Override
     public PsiElement handleElementRename(String newElementName) throws IncorrectOperationException {
-        throw new IncorrectOperationException("Rename cannot be performed for " + getClass());
+        String templatePath = ExpressionEngineUtil.toTemplatePath(getValue());
+
+        if (templatePath != null) {
+            String[] templateParts = templatePath.split("/");
+            templateParts[templateParts.length - 1] = newElementName;
+            String newTemplateName = ExpressionEngineUtil.toTemplateName(StringUtil.join(templateParts, "/"));
+            if (newTemplateName != null) {
+                return element.setName(newTemplateName);
+            }
+        }
+
+        return element;
     }
 
     @Override
