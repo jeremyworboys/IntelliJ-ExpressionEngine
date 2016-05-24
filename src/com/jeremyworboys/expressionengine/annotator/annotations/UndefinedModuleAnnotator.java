5b53a6fce3b10cb096ad1a0f2e621b1a04404056
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-24 22:44:08 +1000

    Add undefined module annotator

---
+++ src/com/jeremyworboys/expressionengine/annotator/annotations/UndefinedModuleAnnotator.java
@@ -0,0 +1,38 @@
+package com.jeremyworboys.expressionengine.annotator.annotations;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.jeremyworboys.expressionengine.pattern.ModuleTagPatterns;
+import com.jeremyworboys.expressionengine.psi.ExpressionEngineModuleOpenTag;
+import com.jeremyworboys.expressionengine.util.module.ModuleIndex;
+import com.jeremyworboys.expressionengine.util.module.ModuleMethod;
+import com.jeremyworboys.expressionengine.util.plugin.PluginIndex;
+import com.jeremyworboys.expressionengine.util.plugin.PluginMethod;
+import org.jetbrains.annotations.NotNull;
+
+public class UndefinedModuleAnnotator implements Annotator {
+    @Override
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        // Skip all elements aren't module open tags
+        if (!ModuleTagPatterns.getModuleOpenTagPattern().accepts(element)) {
+            return;
+        }
+
+        ExpressionEngineModuleOpenTag moduleOpenTag = (ExpressionEngineModuleOpenTag) element;
+        String moduleName = moduleOpenTag.getModuleName();
+
+        Project project = element.getProject();
+        ModuleMethod moduleMethod = ModuleIndex.getInstance(project).getMethod(moduleName);
+        if (moduleMethod != null) {
+            return;
+        }
+        PluginMethod pluginMethod = PluginIndex.getInstance(project).getMethod(moduleName);
+        if (pluginMethod != null) {
+            return;
+        }
+
+        holder.createWarningAnnotation(moduleOpenTag, "Unknown Module or Plugin");
+    }
+}
