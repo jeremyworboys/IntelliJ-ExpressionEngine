4133fac2cc653659c3b4935cd894359958f9a0be
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-24 11:09:47 +1000

    Add get param name helper

--- src/com/jeremyworboys/expressionengine/psi/ExpressionEngineTagParamElement.java
+++ src/com/jeremyworboys/expressionengine/psi/ExpressionEngineTagParamElement.java
@@ -3,4 +3,5 @@ package com.jeremyworboys.expressionengine.psi;
 import com.intellij.psi.PsiNamedElement;
 
 public interface ExpressionEngineTagParamElement extends PsiNamedElement {
+    String getTagParamName();
 }
--- src/com/jeremyworboys/expressionengine/psi/impl/ExpressionEngineTagParamMixin.java
+++ src/com/jeremyworboys/expressionengine/psi/impl/ExpressionEngineTagParamMixin.java
@@ -3,10 +3,16 @@ package com.jeremyworboys.expressionengine.psi.impl;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
 import com.jeremyworboys.expressionengine.psi.ExpressionEngineTagParamElement;
+import com.jeremyworboys.expressionengine.psi.ExpressionEngineTypes;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class ExpressionEngineTagParamMixin extends ASTWrapperPsiElement implements ExpressionEngineTagParamElement {
     public ExpressionEngineTagParamMixin(@NotNull ASTNode node) {
         super(node);
+    }
+
+    @Override
+    public String getTagParamName() {
+        return this.getNode().findChildByType(ExpressionEngineTypes.T_PARAM_NAME).getText();
     }
 }
