ef76eac2074cea50311ff864674c0beef9d82d31
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-24 11:09:56 +1000

    Add get module name helper

--- src/com/jeremyworboys/expressionengine/psi/ExpressionEngineModuleOpenTagElement.java
+++ src/com/jeremyworboys/expressionengine/psi/ExpressionEngineModuleOpenTagElement.java
@@ -3,4 +3,5 @@ package com.jeremyworboys.expressionengine.psi;
 import com.intellij.psi.PsiElement;
 
 public interface ExpressionEngineModuleOpenTagElement extends PsiElement {
+    String getModuleName();
 }
