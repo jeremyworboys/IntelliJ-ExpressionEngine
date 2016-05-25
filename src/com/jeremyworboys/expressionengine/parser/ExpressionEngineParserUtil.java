69f345d3f784385e52a0eb14340fff9ab6ff6554
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-25 17:24:13 +1000

    Add parser util

---
+++ src/com/jeremyworboys/expressionengine/parser/ExpressionEngineParserUtil.java
@@ -0,0 +1,6 @@
+package com.jeremyworboys.expressionengine.parser;
+
+import com.intellij.lang.parser.GeneratedParserUtilBase;
+
+public class ExpressionEngineParserUtil extends GeneratedParserUtilBase {
+}
