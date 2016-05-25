13d84ce923ad7b66e747950a5556ed930b0ae03f
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-25 11:27:30 +1000

    Add module param name variants

--- src/com/jeremyworboys/expressionengine/referencing/ModuleParamNameReference.java
+++ src/com/jeremyworboys/expressionengine/referencing/ModuleParamNameReference.java
@@ -1,16 +1,22 @@
 package com.jeremyworboys.expressionengine.referencing;
 
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReferenceBase;
 import com.jeremyworboys.expressionengine.psi.ExpressionEngineModuleOpenTag;
 import com.jeremyworboys.expressionengine.psi.ExpressionEngineTagParamElement;
+import com.jeremyworboys.expressionengine.util.TagMethodBase;
 import com.jeremyworboys.expressionengine.util.module.ModuleIndex;
 import com.jeremyworboys.expressionengine.util.module.ModuleMethod;
 import com.jeremyworboys.expressionengine.util.plugin.PluginIndex;
 import com.jeremyworboys.expressionengine.util.plugin.PluginMethod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModuleParamNameReference extends PsiReferenceBase<PsiElement> {
     private final ExpressionEngineTagParamElement element;
@@ -28,12 +34,12 @@ public class ModuleParamNameReference extends PsiReferenceBase<PsiElement> {
             return null;
         }
 
-        ModuleMethod moduleMethod = ModuleIndex.getInstance(element.getProject()).getMethod(moduleOpenTag.getModuleName());
+        ModuleMethod moduleMethod = moduleIndex().getMethod(moduleOpenTag.getModuleName());
         if (moduleMethod != null) {
             return moduleMethod.getParameterName(element.getTagParamName());
         }
 
-        PluginMethod pluginMethod = PluginIndex.getInstance(element.getProject()).getMethod(moduleOpenTag.getModuleName());
+        PluginMethod pluginMethod = pluginIndex().getMethod(moduleOpenTag.getModuleName());
         if (pluginMethod != null) {
             return pluginMethod.getParameterName(element.getTagParamName());
         }
@@ -44,7 +50,34 @@ public class ModuleParamNameReference extends PsiReferenceBase<PsiElement> {
     @NotNull
     @Override
     public Object[] getVariants() {
-        // TODO: Module param variants
-        return new Object[0];
+        ExpressionEngineModuleOpenTag moduleOpenTag = element.getModuleOpenTag();
+        if (moduleOpenTag == null) {
+            return new Object[0];
+        }
+
+        TagMethodBase tagMethod = moduleIndex().getMethod(moduleOpenTag.getModuleName());
+        if (tagMethod == null) {
+            tagMethod = pluginIndex().getMethod(moduleOpenTag.getModuleName());
+        }
+        if (tagMethod == null) {
+            return new Object[0];
+        }
+
+        List<LookupElement> variants = new ArrayList<>();
+        for (String paramName : tagMethod.getParameterNames()) {
+            variants.add(LookupElementBuilder.create(paramName));
+        }
+
+        return variants.toArray();
+    }
+
+    @NotNull
+    private PluginIndex pluginIndex() {
+        return PluginIndex.getInstance(element.getProject());
+    }
+
+    @NotNull
+    private ModuleIndex moduleIndex() {
+        return ModuleIndex.getInstance(element.getProject());
     }
 }
