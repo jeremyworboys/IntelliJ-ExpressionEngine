f0a9cbad2daaf259bfec02e0661cd860f15ec565
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-25 12:02:56 +1000

    Mark module name as not null

--- src/com/jeremyworboys/expressionengine/psi/ExpressionEngineModuleOpenTagElement.java
+++ src/com/jeremyworboys/expressionengine/psi/ExpressionEngineModuleOpenTagElement.java
@@ -1,7 +1,9 @@
 package com.jeremyworboys.expressionengine.psi;
 
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
 
 public interface ExpressionEngineModuleOpenTagElement extends PsiElement {
+    @NotNull
     String getModuleName();
 }
