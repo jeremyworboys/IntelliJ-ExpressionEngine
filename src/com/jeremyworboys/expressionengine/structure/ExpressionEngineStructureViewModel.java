e399743b33a98fd0b9711fc43a8c9a64d2f75e1e
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-25 12:40:25 +1000

    Use alpha sorter

--- src/com/jeremyworboys/expressionengine/structure/ExpressionEngineStructureViewModel.java
+++ src/com/jeremyworboys/expressionengine/structure/ExpressionEngineStructureViewModel.java
@@ -3,12 +3,19 @@ package com.jeremyworboys.expressionengine.structure;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.StructureViewModelBase;
 import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
 public class ExpressionEngineStructureViewModel extends StructureViewModelBase implements StructureViewModel.ElementInfoProvider {
     public ExpressionEngineStructureViewModel(@NotNull PsiFile psiFile) {
         super(psiFile, new ExpressionEngineStructureViewElement(psiFile));
+    }
+
+    @NotNull
+    @Override
+    public Sorter[] getSorters() {
+        return new Sorter[]{Sorter.ALPHA_SORTER};
     }
 
     @Override
