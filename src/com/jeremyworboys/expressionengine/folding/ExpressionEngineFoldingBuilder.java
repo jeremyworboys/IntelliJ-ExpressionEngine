7c3bcb8dd7f7429455f1da0007ee5089ebd79db8
Jeremy Worboys <jw@jeremyworboys.com>
2016-05-25 11:49:27 +1000

    Add folding builder

---
+++ src/com/jeremyworboys/expressionengine/folding/ExpressionEngineFoldingBuilder.java
@@ -0,0 +1,28 @@
+package com.jeremyworboys.expressionengine.folding;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.folding.FoldingBuilderEx;
+import com.intellij.lang.folding.FoldingDescriptor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ExpressionEngineFoldingBuilder extends FoldingBuilderEx {
+    @NotNull
+    @Override
+    public FoldingDescriptor[] buildFoldRegions(@NotNull PsiElement root, @NotNull Document document, boolean quick) {
+        return new FoldingDescriptor[0];
+    }
+
+    @Nullable
+    @Override
+    public String getPlaceholderText(@NotNull ASTNode node) {
+        return null;
+    }
+
+    @Override
+    public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+        return false;
+    }
+}
