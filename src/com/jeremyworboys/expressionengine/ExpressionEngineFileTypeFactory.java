63b12403c2674db7b68e154ce07c40606dc132f5
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 20:32:09 +1000

    Remove multiple file extensions for now

--- src/com/jeremyworboys/expressionengine/ExpressionEngineFileTypeFactory.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineFileTypeFactory.java
@@ -2,7 +2,6 @@ package com.jeremyworboys.expressionengine;
 
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
-import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class ExpressionEngineFileTypeFactory extends FileTypeFactory {
@@ -10,6 +9,6 @@ public class ExpressionEngineFileTypeFactory extends FileTypeFactory {
     public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
         fileTypeConsumer.consume(
             ExpressionEngineFileType.INSTANCE,
-            StringUtil.join(ExpressionEngineFileType.INSTANCE.getExtensions(), FileTypeConsumer.EXTENSION_DELIMITER));
+            ExpressionEngineFileType.INSTANCE.getDefaultExtension());
     }
 }
