03d11620fee3f00be8275f1b5e75df9e8c50e698
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-26 00:23:21 +1000

    Simplify project component

--- src/com/jeremyworboys/expressionengine/ExpressionEngineProjectComponent.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineProjectComponent.java
@@ -1,37 +1,14 @@
 package com.jeremyworboys.expressionengine;
 
-import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ExpressionEngineProjectComponent implements ProjectComponent {
-    @Override
-    public void projectOpened() {
-
-    }
-
-    @Override
-    public void projectClosed() {
-
-    }
-
-    @Override
-    public void initComponent() {
-
-    }
-
-    @Override
-    public void disposeComponent() {
-
-    }
-
-    @NotNull
-    @Override
-    public String getComponentName() {
-        return "ExpressionEngineProjectComponent";
+public class ExpressionEngineProjectComponent extends AbstractProjectComponent {
+    protected ExpressionEngineProjectComponent(Project project) {
+        super(project);
     }
 
     public static boolean isEnabled(Project project) {
