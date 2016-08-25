66f1c8771234531e700df14c0f43fc8fa0c1c987
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 21:06:00 +1000

    Use explicit filename matcher

--- src/com/jeremyworboys/expressionengine/ExpressionEngineFileTypeFactory.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineFileTypeFactory.java
@@ -3,12 +3,13 @@ package com.jeremyworboys.expressionengine;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.model.fileTypes.FileNameMatcherFactory;
 
 public class ExpressionEngineFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
         fileTypeConsumer.consume(
             ExpressionEngineFileType.INSTANCE,
-            ExpressionEngineFileType.INSTANCE.getDefaultExtension());
+            FileNameMatcherFactory.getInstance().createMatcher("*." + ExpressionEngineFileType.INSTANCE.getDefaultExtension()));
     }
 }
