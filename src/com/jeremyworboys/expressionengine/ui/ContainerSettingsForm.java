2cd9c033f29ce27cebb7a3c343eab039e22680ec
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-26 09:13:07 +1000

    Create container settings form

---
+++ src/com/jeremyworboys/expressionengine/ui/ContainerSettingsForm.java
@@ -0,0 +1,65 @@
+package com.jeremyworboys.expressionengine.ui;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class ContainerSettingsForm implements Configurable {
+
+    private final Project project;
+
+    private JPanel mainPanel;
+    private JPanel listViewPanel;
+    private JButton resetButton;
+
+    public ContainerSettingsForm(@NotNull Project project) {
+        this.project = project;
+    }
+
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return "Container";
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        // TODO: Implement createComponent() method...
+        return this.mainPanel;
+    }
+
+    @Override
+    public boolean isModified() {
+        // TODO: Implement isModified() method...
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+        // TODO: Implement apply() method...
+
+    }
+
+    @Override
+    public void reset() {
+        // TODO: Implement reset() method...
+
+    }
+
+    @Override
+    public void disposeUIResources() {
+
+    }
+}
