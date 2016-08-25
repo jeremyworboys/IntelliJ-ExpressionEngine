7c863194ee9b70eb9332b5ab01f566063081fe3f
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 15:37:33 +1000

    Add project component class

---
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineProjectComponent.java
@@ -0,0 +1,32 @@
+package com.jeremyworboys.expressionengine;
+
+import com.intellij.openapi.components.ProjectComponent;
+import org.jetbrains.annotations.NotNull;
+
+public class ExpressionEngineProjectComponent implements ProjectComponent {
+    @Override
+    public void projectOpened() {
+
+    }
+
+    @Override
+    public void projectClosed() {
+
+    }
+
+    @Override
+    public void initComponent() {
+
+    }
+
+    @Override
+    public void disposeComponent() {
+
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return "ExpressionEngineProjectComponent";
+    }
+}
