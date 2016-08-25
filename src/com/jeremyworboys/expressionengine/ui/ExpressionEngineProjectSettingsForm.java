726454669e0b3fb465b4bfdccb010862213ff849
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 16:23:30 +1000

    Build settings form

--- src/com/jeremyworboys/expressionengine/ui/ExpressionEngineProjectSettingsForm.java
+++ src/com/jeremyworboys/expressionengine/ui/ExpressionEngineProjectSettingsForm.java
@@ -1,4 +1,65 @@
 package com.jeremyworboys.expressionengine.ui;
 
-public class ExpressionEngineProjectSettingsForm {
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import com.jeremyworboys.expressionengine.ExpressionEngineSettings;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class ExpressionEngineProjectSettingsForm implements Configurable {
+
+    private final Project project;
+
+    private JPanel panel;
+    private JCheckBox enabled;
+
+    public ExpressionEngineProjectSettingsForm(@NotNull final Project project) {
+        this.project = project;
+    }
+
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return "ExpressionEngine Plugin";
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        return panel;
+    }
+
+    @Override
+    public boolean isModified() {
+        return !getSettings().pluginEnabled == enabled.isSelected();
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+        getSettings().pluginEnabled = enabled.isSelected();
+    }
+
+    @Override
+    public void reset() {
+        enabled.setSelected(getSettings().pluginEnabled);
+    }
+
+    @Override
+    public void disposeUIResources() {
+
+    }
+
+    private ExpressionEngineSettings getSettings() {
+        return ExpressionEngineSettings.getInstance(project);
+    }
 }
