fe40ddd9552e1357cc9a98182899e7295253d9aa
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 23:42:43 +1000

    Add file view provider factory

---
+++ src/com/jeremyworboys/expressionengine/template/ExpressionEngineFileViewProviderFactory.java
@@ -0,0 +1,19 @@
+package com.jeremyworboys.expressionengine.template;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.FileViewProviderFactory;
+import com.intellij.psi.PsiManager;
+import org.jetbrains.annotations.NotNull;
+
+public class ExpressionEngineFileViewProviderFactory implements FileViewProviderFactory{
+    public ExpressionEngineFileViewProviderFactory() {
+    }
+
+    @NotNull
+    @Override
+    public FileViewProvider createFileViewProvider(@NotNull VirtualFile file, Language language, @NotNull PsiManager manager, boolean eventSystemEnabled) {
+        return new ExpressionEngineFileViewProvider(manager, file, eventSystemEnabled);
+    }
+}
