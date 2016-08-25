645dab53538127bbe065446f1538581970f82a8b
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 20:21:18 +1000

    Implement template language

--- src/com/jeremyworboys/expressionengine/ExpressionEngineLanguage.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineLanguage.java
@@ -1,8 +1,9 @@
 package com.jeremyworboys.expressionengine;
 
 import com.intellij.lang.Language;
+import com.intellij.psi.templateLanguages.TemplateLanguage;
 
-public class ExpressionEngineLanguage extends Language {
+public class ExpressionEngineLanguage extends Language implements TemplateLanguage {
     public static final ExpressionEngineLanguage INSTANCE = new ExpressionEngineLanguage();
 
     private ExpressionEngineLanguage() {
