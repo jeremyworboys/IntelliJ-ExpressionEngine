097d9ae3f2c56a657df9ce0a20d16a20162e552e
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 15:42:47 +1000

    Implement persistent state component

--- src/com/jeremyworboys/expressionengine/ExpressionEngineSettings.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineSettings.java
@@ -1,4 +1,18 @@
 package com.jeremyworboys.expressionengine;
 
-public class ExpressionEngineSettings {
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+
+@State(name = "ExpressionEngineSettings")
+public class ExpressionEngineSettings implements PersistentStateComponent<ExpressionEngineSettings> {
+    @Override
+    public ExpressionEngineSettings getState() {
+        return this;
+    }
+
+    @Override
+    public void loadState(ExpressionEngineSettings state) {
+        XmlSerializerUtil.copyBean(state, this);
+    }
 }
