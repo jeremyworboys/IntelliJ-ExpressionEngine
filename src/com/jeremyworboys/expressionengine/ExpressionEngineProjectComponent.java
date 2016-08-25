e60c8993277f6b6639c943af59b10b510cb6dcd7
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 15:53:18 +1000

    Add get settings to project component

--- src/com/jeremyworboys/expressionengine/ExpressionEngineProjectComponent.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineProjectComponent.java
@@ -1,7 +1,11 @@
 package com.jeremyworboys.expressionengine;
 
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ExpressionEngineProjectComponent implements ProjectComponent {
     @Override
@@ -28,5 +32,14 @@ public class ExpressionEngineProjectComponent implements ProjectComponent {
     @Override
     public String getComponentName() {
         return "ExpressionEngineProjectComponent";
+    }
+
+    public static boolean isEnabled(Project project) {
+        return ExpressionEngineSettings.getInstance(project).pluginEnabled;
+    }
+
+    @Contract("null -> false")
+    public static boolean isEnabled(@Nullable PsiElement psiElement) {
+        return psiElement != null && isEnabled(psiElement.getProject());
     }
 }
