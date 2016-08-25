e65555496b4db715dd9f96bd389f73c293e6410b
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-25 15:52:05 +1000

    Add get instance method to settings

--- src/com/jeremyworboys/expressionengine/ExpressionEngineSettings.java
+++ src/com/jeremyworboys/expressionengine/ExpressionEngineSettings.java
@@ -1,13 +1,23 @@
 package com.jeremyworboys.expressionengine;
 
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
+import com.intellij.openapi.project.Project;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 
 @State(name = "ExpressionEngineSettings")
 public class ExpressionEngineSettings implements PersistentStateComponent<ExpressionEngineSettings> {
 
     public boolean pluginEnabled = false;
+
+    protected Project project;
+
+    public static ExpressionEngineSettings getInstance(Project project) {
+        ExpressionEngineSettings settings = ServiceManager.getService(project, ExpressionEngineSettings.class);
+        settings.project = project;
+        return settings;
+    }
 
     @Override
     public ExpressionEngineSettings getState() {
