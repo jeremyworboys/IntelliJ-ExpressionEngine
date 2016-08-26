dcde1ee0b7d40789a88db982f24f9b2e71243241
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-26 22:24:15 +1000

    Remove useless container settings page

--- src/com/jeremyworboys/expressionengine/container/ContainerHelper.java
+++ src/com/jeremyworboys/expressionengine/container/ContainerHelper.java
@@ -1,14 +1,8 @@
 package com.jeremyworboys.expressionengine.container;
 
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VfsUtil;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.jeremyworboys.expressionengine.ExpressionEngineProjectComponent;
-import com.jeremyworboys.expressionengine.ExpressionEngineSettings;
 import com.jeremyworboys.expressionengine.model.ModelSerializable;
 import com.jeremyworboys.expressionengine.model.SerializableModel;
 import com.jeremyworboys.expressionengine.util.PhpElementsUtil;
@@ -21,44 +15,6 @@ import org.jetbrains.annotations.Nullable;
 import java.util.*;
 
 public class ContainerHelper {
-
-    @NotNull
-    public static List<ContainerFile> getContainerFiles(@NotNull Project project) {
-        List<ContainerFile> containerFiles = new ArrayList<>();
-
-        // EE2 does not have container files
-        if (!ExpressionEngineProjectComponent.isEnabledVersion3(project)) {
-            return containerFiles;
-        }
-
-        String systemPath = ExpressionEngineSettings.getInstance(project).systemPath;
-        VirtualFile systemDirectory = LocalFileSystem.getInstance().findFileByPath(systemPath);
-
-        // Look for app setup file
-        String appSetupPath = "ee/EllisLab/ExpressionEngine/app.setup.php";
-        VirtualFile appSetupFile = VfsUtil.findRelativeFile(systemDirectory, appSetupPath.split("/"));
-        if (appSetupFile != null) {
-            containerFiles.add(new ContainerFile(appSetupFile.getPath()/*, ContainerFileIndex.MAIN, ContainerFileIndex.NamespaceType.BUNDLE*/));
-        }
-
-        // Look for addon setup files
-        String[] addonPaths = {"ee/EllisLab/Addons", "user/addons"};
-        for (String addonPath : addonPaths) {
-            VirtualFile firstPartyAddonsDirectory = VfsUtil.findRelativeFile(systemDirectory, addonPath.split("/"));
-            if (firstPartyAddonsDirectory != null) {
-                for (VirtualFile firstPartyAddonDirectory : firstPartyAddonsDirectory.getChildren()) {
-                    if (firstPartyAddonDirectory.isDirectory()) {
-                        VirtualFile firstPartyAddonSetupFile = VfsUtil.findRelativeFile(firstPartyAddonDirectory, "addon.setup.php");
-                        if (firstPartyAddonSetupFile != null) {
-                            containerFiles.add(new ContainerFile(firstPartyAddonSetupFile.getPath()/*, ContainerFileIndex.MAIN, ContainerFileIndex.NamespaceType.BUNDLE*/));
-                        }
-                    }
-                }
-            }
-        }
-
-        return containerFiles;
-    }
 
     @NotNull
     public static List<ServiceSerializable> getServicesInFile(PsiFile psiFile, String servicePrefix) {
