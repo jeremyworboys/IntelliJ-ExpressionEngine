ddb1f8a8d4b8a33b3a6e54b1efa53abf7c3fb04b
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-26 14:53:16 +1000

    Implement methods

--- src/com/jeremyworboys/expressionengine/container/model/SerializableModel.java
+++ src/com/jeremyworboys/expressionengine/container/model/SerializableModel.java
@@ -1,4 +1,27 @@
 package com.jeremyworboys.expressionengine.container.model;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class SerializableModel implements ModelSerializable {
+    private String id;
+    private String className;
+
+    public SerializableModel(@NotNull String id) {
+        this.id = id;
+    }
+
+    @NotNull
+    public String getId() {
+        return id;
+    }
+
+    @Nullable
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 }
